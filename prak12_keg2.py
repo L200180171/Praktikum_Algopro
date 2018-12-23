767d28a53a186d7affb19f3ff4c9fe48d3db3bae
L200180171 <44251251+L200180171@users.noreply.github.com>
2018-12-23 21:03:06 +0700

    Add files via upload

---
+++ prak12_keg2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print "<!DOCTYPE html>"
+print
+print """ <html>
+<head><title>Selamat Datang</title></head>
+<body>
+<table>
+	<tr>
+		<td rowspan='6'>FOTO</td>
+		<td><b><font size='5'>Data diri</font><b></td>
+	</tr>
+	<tr>
+		<td>Nama </td>
+		<td>:</td>
+		<td>Muhammad Rizqi Erdyansyah</td>
+	</tr>
+	<tr>
+		<td>Alamat tinggal </td>
+		<td>:</td>
+		<td>Surakarta</td>
+	</tr>
+	<tr>
+		<td>Tempat, tanggal lahir </td>
+		<td>:</td>
+		<td>Surakarta, 05 Oktober 2000</td>
+	</tr>
+	<tr>
+		<td>Tempat wisata favorit </td>
+		<td>:</td>
+		<td>Jogja</td>
+	</tr>
+	<tr>
+		<td>Motto </td>
+		<td>:</td>
+		<td>Future</td>
+	</tr>
+</table>
+</body>
+</html> """
